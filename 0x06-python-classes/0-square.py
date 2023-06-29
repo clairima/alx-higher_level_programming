8c2fc362a9953b1b60f98bf22c1bedfbbde711be
clairima <mertahrima@gmail.com>
2023-06-29 10:36:54 -0700

    0 square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -6,36 +6,4 @@ class Square:
     Attributes:
         None
     """
-
-    def __init__(self):
-        """
-        Initializes a new instance of the Square class.
-
-        Parameters:
-            None
-        """
-        pass
-
-    def calculate_area(self):
-        """
-        Calculates the area of the square.
-
-        Parameters:
-            None
-
-        Returns:
-            The area of the square.
-        """
-        pass
-
-    def calculate_perimeter(self):
-        """
-        Calculates the perimeter of the square.
-
-        Parameters:
-            None
-
-        Returns:
-            The perimeter of the square.
-        """
-        pass
+    pass
