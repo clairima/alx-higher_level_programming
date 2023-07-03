d78b0f6ab7e7524a9e0d40a7f000fc7a4c5837cc
clairima <mertahrima@gmail.com>
2023-07-03 03:26:55 -0700

    0 rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -3,8 +3,5 @@ class Rectangle:
 
     """
     This class represents a rectangle.
-    Attributes:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
     """
     pass
