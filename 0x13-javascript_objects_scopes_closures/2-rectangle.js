66d55dfcf6a9f48efd369ec879d6f6af21466c4d
clairima <mertahrima@gmail.com>
2023-09-12 12:52:20 -0700

    tasks 8 9 10

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,7 +3,9 @@ class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0) {
       return;
-    }
+    } else if (w == undefined || h == undefined) {
+        return;
+      }
     this.width = w;
     this.height = h;
   }
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,10 +3,6 @@
 const OldSquare = require('./5-square');
 
 class Square extends OldSquare {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
