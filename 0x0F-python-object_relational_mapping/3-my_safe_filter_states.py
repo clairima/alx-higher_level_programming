d33f3f28ac90aaea7c499773a4e7da5b1665e389
clairima <mertahrima@gmail.com>
2023-09-15 09:45:42 -0700

    2 3 filter

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""takes an argument and displays all values in the states table where name
-matches the argument from the database hbtn_0e_0_usa"""
+"""
+takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa
+where name matches the argument
+and is safe from SQL injections
+"""
 
 if __name__ == '__main__':
 
@@ -11,8 +15,8 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
-                ORDER BY states.id".format(sys.argv[4]))
+    cursor.execute("SELECT * FROM states WHERE name=%s\
+                ORDER BY states.id", (sys.argv[4],))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
