f7715af4eef5d2f269e4122a954127b12cf51d0b
clairima <mertahrima@gmail.com>
2023-09-29 12:16:34 -0700

    hbtn status 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 """
 This module fetches https://alx-intranet.hbtn.io/status using urllib.
-It prints the response body information including type, content, and UTF-8 content.
+It prints the response body information including type,
+content, and UTF-8 content.
 """
 
 import urllib.request
 
-def fetch_url(url):
-    """
-    Fetches the given URL and prints response body information.
-    Args:
-        url (str): The URL to fetch.
-    """
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        utf8_content = body.decode('utf-8')
 
-    print("Body response:")
-    print("    - type:", type(body))
-    print("    - content:", body)
-    print("    - utf8 content:", utf8_content)
+def fetch_url():
+    """
+    Fetches URL and prints response body information.
+    """
+    with urllib.request.urlopen(
+        "https://alx-intranet.hbtn.io/status"
+    ) as response:
+        body = response.read()
+        print("Body response:")
+        print("    - type: {}".format(type(body)))
+        print("    - content: {}".format(body))
+        print("    - utf8 content: {}".format(body.decode("utf-8")))
+
 
 if __name__ == "__main__":
-    fetch_url("https://alx-intranet.hbtn.io/status")
+    fetch_url()
