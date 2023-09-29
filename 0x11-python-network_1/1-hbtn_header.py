db00ad7cbcf2d4b70afcde248d0b5f9ca431801d
clairima <mertahrima@gmail.com>
2023-09-29 12:24:09 -0700

    header 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 """
-This module takes a URL as input, sends a request to the URL, and displays the value
+This module takes a URL as input, sends a request to the URL,
+and displays the value
 of the X-Request-Id variable found in the header of the response.
 """
 
 import urllib.request
 import sys
 
+
 def fetch_x_request_id(url):
     """
-    Fetches the given URL and prints the value of X-Request-Id from the response header.
+    Fetches the given URL and prints the value of X-Request-Id
+    from the response header.
     Args:
         url (str): The URL to send the request to.
     """
     with urllib.request.urlopen(url) as response:
         x_request_id = response.getheader('X-Request-Id')
         print(x_request_id)
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
